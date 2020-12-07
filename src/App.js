71aaadadf6de994f912bfec1251684e5814f4fdb
David Silaghi <david.silaghi1507@gmail.com>
2020-12-07 14:08:04 +0200

    react router deployment fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 
 import MoviesGrid from './components/MoviesGrid'
 import MovieDetails from './pages/MovieDetails'
