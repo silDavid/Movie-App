8279f2ce6b793bddd96a1a2bca4a6d8e64b1e75c
David Silaghi <david.silaghi1507@gmail.com>
2020-12-08 22:59:28 +0200

    overview animation/ movie cards size - updates

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -9,7 +9,7 @@ const MoviesGrid = ({ movies }) => {
                 <div key={key} className="movie-card m-3 mb-5">
                     <Link to={`/details/${movie.imdbID}`}>
                         <img src={movie.Poster} alt={movie.Title}></img>
-                        <div className="movie-info align-items-center d-flex justify-content-center">
+                        <div className="movie-info movie-info--blur align-items-center d-flex justify-content-center p-2">
                             <h4 className="p-2">{movie.Title}</h4>
                             <p className="p-2"><small>{movie.Year}</small></p>
                         </div>
